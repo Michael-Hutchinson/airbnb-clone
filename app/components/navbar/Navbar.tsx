a3ecd588eb8834eaa59521c1c80b9259aff7486d
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-03-31 23:50:33 +0100

    feat: start working on navbar component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,18 @@
+import React from 'react';
+import Container from '../Container';
+
 const Navbar = () => {
-  return <div>I am a navbar!</div>;
+  return (
+    <div className='fixed w-full bg-white z-10 shadow-sm'>
+      <div className='py-4 border-b-[1px]'>
+        <Container>
+          <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
+            nav
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
