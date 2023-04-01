5f439f4f5522f4b5175ea45c4da9b03c8c9e90bb
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-04-01 12:07:41 +0100

    fix: update container import

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Container from '../Container';
+import Container from '../container/Container';
 import Logo from './logo/Logo';
 import Search from './search/Search';
 import UserMenu from './usermenu/UserMenu';
