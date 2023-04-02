2b8e601d5066462f903e2800d75fa6a73a823000
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-04-02 17:07:53 +0100

    feat: start working on modal component
    - added props

---
+++ app/components/modals/Modal.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from 'react';
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: ReactElement;
+  footer?: ReactElement;
+  actionLabel: string;
+  disabled: boolean;
+  secondaryAction?: () => void;
+  secondaryLabel?: string;
+}
+
+const Modal = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secondaryAction,
+  secondaryLabel,
+}: ModalProps) => {
+  return <div>Modal</div>;
+};
+
+export default Modal;
