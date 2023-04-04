1571a6a8c93f685d5b9c0b4186f923daca6e16db
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-04-04 18:25:57 +0100

    feat: start building register modal

--- app/components/modals/RegisterModal/RegisterModal.tsx
+++ app/components/modals/RegisterModal/RegisterModal.tsx
@@ -1,5 +1,29 @@
 'use client';
 
-const RegisterModal = () => <div>Register Modal</div>;
+import React, { useCallback, useState } from 'react';
+import axios from 'axios';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  return <div>Register Modal</div>;
+};
 
 export default RegisterModal;
