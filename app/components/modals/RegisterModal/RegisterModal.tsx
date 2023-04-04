7aca149a760e20a112e85a5c756b344576354255
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-04-04 18:29:47 +0100

    feat: catch error in console log for now

--- app/components/modals/RegisterModal/RegisterModal.tsx
+++ app/components/modals/RegisterModal/RegisterModal.tsx
@@ -26,9 +26,14 @@ const RegisterModal = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
-    axios.post('/api/register', data).then(() => {
-      registerModal.onClose();
-    });
+    axios
+      .post('/api/register', data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return <div>Register Modal</div>;
