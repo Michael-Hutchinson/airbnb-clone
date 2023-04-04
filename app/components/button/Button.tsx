a0e7d6550040545432a731aaec89e6484ad6a1bc
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-04-04 17:49:23 +0100

    feat: finish button
    - add on click
    - add disabled
    - add icon

--- app/components/button/Button.tsx
+++ app/components/button/Button.tsx
@@ -16,10 +16,12 @@ const Button = ({
   disabled,
   outline,
   small,
-  icon,
+  icon: Icon,
 }: ButtonProps) => {
   return (
     <button
+      onClick={onClick}
+      disabled={disabled}
       className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full 
       ${outline ? 'bg-white' : 'bg-rose-500'} 
       ${outline ? 'border-black' : 'border-rose-500'} 
@@ -29,6 +31,7 @@ const Button = ({
       ${small ? 'font-light' : 'font-semibold'} 
       ${small ? 'border-[1px]' : 'border-2'}`}
     >
+      {Icon && <Icon size={24} className='absolute left-4 top-3' />}
       {label}
     </button>
   );
