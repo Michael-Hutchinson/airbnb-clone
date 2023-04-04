8dc44d47ac9808b0eaed100e185be41568f07016
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-04-04 17:56:29 +0100

    feat: create secondary action button

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -14,7 +14,7 @@ interface ModalProps {
   actionLabel: string;
   disabled: boolean;
   secondaryAction?: () => void;
-  secondaryLabel?: string;
+  secondaryActionLabel?: string;
 }
 
 const Modal = ({
@@ -27,7 +27,7 @@ const Modal = ({
   actionLabel,
   disabled,
   secondaryAction,
-  secondaryLabel,
+  secondaryActionLabel,
 }: ModalProps) => {
   const [showModal, setShowModal] = useState(isOpen);
 
@@ -87,6 +87,14 @@ const Modal = ({
             <div className='relative p-6 flex-auto'>{body}</div>
             <div className='flex flex-col gap-2 p-6'>
               <div className='flex flex-row items-center gap-4 w-full'>
+                {secondaryAction && secondaryActionLabel && (
+                  <Button
+                    outline
+                    disabled={disabled}
+                    label={secondaryActionLabel}
+                    onClick={handleSecondaryAction}
+                  />
+                )}
                 <Button
                   disabled={disabled}
                   label={actionLabel}
