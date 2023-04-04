c56443fe83c748f4634bea3af5b64a3bec1462fa
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-04-04 17:37:20 +0100

    feat: add interface for button props to be used

--- app/components/button/Button.tsx
+++ app/components/button/Button.tsx
@@ -1,5 +1,24 @@
-const Button = () => {
-  return <button>Button</button>;
+import React, { MouseEvent } from 'react';
+import { IconType } from 'react-icons';
+
+interface ButtonProps {
+  label: string;
+  onClick: (e: MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+  icon?: IconType;
+}
+
+const Button = ({
+  label,
+  onClick,
+  disabled,
+  outline,
+  small,
+  icon,
+}: ButtonProps) => {
+  return <button>{label}</button>;
 };
 
 export default Button;
