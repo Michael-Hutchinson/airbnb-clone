0ecfa1478581d4ea314806e5702a0d2d660d9ec3
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-04-04 17:51:54 +0100

    feat: add button props to modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -87,7 +87,11 @@ const Modal = ({
             <div className='relative p-6 flex-auto'>{body}</div>
             <div className='flex flex-col gap-2 p-6'>
               <div className='flex flex-row items-center gap-4 w-full'>
-                <Button />
+                <Button
+                  disabled={disabled}
+                  label={actionLabel}
+                  onClick={handleSubmit}
+                />
               </div>
             </div>
           </div>
