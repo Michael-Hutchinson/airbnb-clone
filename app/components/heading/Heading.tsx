309ea638c56d4eb189274c4564ce3bb084b42bbc
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-04-05 19:03:20 +0100

    feat: add interface props

--- app/components/heading/Heading.tsx
+++ app/components/heading/Heading.tsx
@@ -1,4 +1,12 @@
-const Heading = () => {
+'use client';
+
+interface HeadingProps {
+  title: string;
+  subtitle?: string;
+  center?: boolean;
+}
+
+const Heading = ({ title, subtitle, center }: HeadingProps) => {
   return <div>Modal Heading</div>;
 };
 
