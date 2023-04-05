bf3a213fbcbc90000d94b449a42f1ad381ededcd
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-04-05 19:05:33 +0100

    feat: finish heading component

--- app/components/heading/Heading.tsx
+++ app/components/heading/Heading.tsx
@@ -7,7 +7,12 @@ interface HeadingProps {
 }
 
 const Heading = ({ title, subtitle, center }: HeadingProps) => {
-  return <div>Modal Heading</div>;
+  return (
+    <div className={center ? 'text-center' : 'text-start'}>
+      <div className='text-2xl font-bold'>{title}</div>
+      <div className='font-light text-neutral-500 mt-2'>{subtitle}</div>
+    </div>
+  );
 };
 
 export default Heading;
