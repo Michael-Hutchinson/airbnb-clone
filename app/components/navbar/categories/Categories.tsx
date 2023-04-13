2586dfab455c8e543d8565691e5517c74b72fd86
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-04-13 19:23:33 +0100

    feat: add selected state for categories

--- app/components/navbar/categories/Categories.tsx
+++ app/components/navbar/categories/Categories.tsx
@@ -5,6 +5,7 @@ import { TbBeach } from 'react-icons/tb';
 import { GiWindmill } from 'react-icons/gi';
 import { MdOutlineVilla } from 'react-icons/md';
 import CategoryBox from '../../categorybox/CategoryBox';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export const categories = [
   {
@@ -24,19 +25,29 @@ export const categories = [
   },
 ];
 
-const Categories = () => (
-  <Container>
-    <div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'>
-      {categories.map((item) => (
-        <CategoryBox
-          key={item.label}
-          label={item.label}
-          description={item.description}
-          icon={item.icon}
-        />
-      ))}
-    </div>
-  </Container>
-);
+const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get('category');
+  const pathname = usePathname();
+  const isMainPage = pathname === '/';
+
+  if (!isMainPage) return null;
+
+  return (
+    <Container>
+      <div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'>
+        {categories.map((item) => (
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            selected={item.label === category}
+            description={item.description}
+            icon={item.icon}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
 
 export default Categories;
