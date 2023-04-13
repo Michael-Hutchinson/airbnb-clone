8891cb6c6d3cec96d4445668e467c32eef0fbc78
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-04-13 18:45:12 +0100

    feat: allow category boxes to be selected
    - add params to url when selected using onclick function

--- app/components/categorybox/CategoryBox.tsx
+++ app/components/categorybox/CategoryBox.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
 import { IconType } from 'react-icons';
+import queryString from 'query-string';
 
 interface CategoryBoxProps {
   label: string;
@@ -9,14 +12,50 @@ interface CategoryBoxProps {
   selected?: boolean;
 }
 
+interface UpdatedQueryType {
+  [key: string]: string | undefined;
+  category?: string;
+}
+
 const CategoryBox = ({
   label,
   description,
   icon: Icon,
   selected,
 }: CategoryBoxProps) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = queryString.parse(params.toString());
+    }
+
+    const updatedQuery: UpdatedQueryType = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get('category') === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = queryString.stringifyUrl(
+      {
+        url: '/',
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
+
   return (
     <div
+      onClick={handleClick}
       className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer 
       ${selected ? 'border-b-neutral-800' : 'border-transparent'}
       ${selected ? 'text-neutral-800' : 'text-neutral-500'}
