dbdcde529a7245ac2dee7e293d804e5e743a9294
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-06-23 14:41:09 +0100

    feat: add classnames to select input

--- app/components/input/CountrySelect/CountrySelect.tsx
+++ app/components/input/CountrySelect/CountrySelect.tsx
@@ -32,10 +32,15 @@ const CountrySelect = ({ value, onChange }: CountrySelectProps) => {
             <div>{option.flag}</div>
             <div>
               {option.label},{' '}
-              <span className='text-neutral-800 ml-1'>{option.region}</span>
+              <span className='text-neutral-500 ml-1'>{option.region}</span>
             </div>
           </div>
         )}
+        classNames={{
+          control: () => 'p-3 border-2',
+          input: () => 'text-lg',
+          option: () => 'text-lg',
+        }}
       />
     </div>
   );
