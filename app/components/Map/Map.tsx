6b487c27b611f9c8bca6cf90f3d72138391fa674
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-06-28 19:41:16 +0100

    feat: add marker for map

--- app/components/Map/Map.tsx
+++ app/components/Map/Map.tsx
@@ -28,10 +28,8 @@ const WorldMap = ({ center }: WorldMapProps) => {
       scrollWheelZoom={false}
       className='h-[35vh] rounded-lg'
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
+      <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+      {center && <Marker position={center as L.LatLngExpression} />}
     </MapContainer>
   );
 };
