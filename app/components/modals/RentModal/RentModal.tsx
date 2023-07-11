961c800ada0ecdd898afea5bcb329253ba35da06
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-11 21:02:54 +0100

    feat: create description and price step

--- app/components/modals/RentModal/RentModal.tsx
+++ app/components/modals/RentModal/RentModal.tsx
@@ -11,6 +11,7 @@ import CountrySelect from '../../input/CountrySelect/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../../input/Counter/Counter';
 import ImageUpload from '../../input/ImageUpload/ImageUpload';
+import Input from '../../input/Input';
 
 enum Steps {
   Category = 0,
@@ -25,6 +26,7 @@ const RentModal = () => {
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(Steps.Category);
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     register,
@@ -173,6 +175,55 @@ const RentModal = () => {
         <ImageUpload
           value={imageSrcs}
           onChange={(value) => setCustomValue('imageSrcs', value)}
+        />
+      </div>
+    );
+  }
+
+  if (step === Steps.Description) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='How would you describe your place?'
+          subtitle='Short and sweet works best!'
+        />
+        <Input
+          id='title'
+          label='Title'
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr />
+        <Input
+          id='description'
+          label='Description'
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    );
+  }
+
+  if (step === Steps.Price) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Now, set your price'
+          subtitle='How much do you charge per night?'
+        />
+        <Input
+          id='price'
+          label='Price'
+          formatPrice
+          type='number'
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
         />
       </div>
     );
