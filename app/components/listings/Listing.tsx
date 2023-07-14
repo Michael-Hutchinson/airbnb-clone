e702e599e8a747052f1380de9b377b640386a9d3
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-14 19:56:10 +0100

    feat: add interface props to get rid of temp warnings

--- app/components/listings/Listing.tsx
+++ app/components/listings/Listing.tsx
@@ -2,6 +2,10 @@
 
 import { SafeListing, SafeUser } from '@/app/types';
 import { Reservation } from '@prisma/client';
+import { useMemo } from 'react';
+import { categories } from '../navbar/categories/categoryItems';
+import Container from '../container/Container';
+import ListingHead from './ListingHead/ListingHead';
 
 interface ListingClientProps {
   reservations?: Reservation[];
@@ -12,10 +16,24 @@ interface ListingClientProps {
 }
 
 const ListingClient = ({ listing, currentUser }: ListingClientProps) => {
+  const category = useMemo(() => {
+    return categories.find((item) => item.label === listing.category);
+  }, [listing.category]);
+
   return (
-    <div>
-      <h1>List</h1>
-    </div>
+    <Container>
+      <div className='max-w-screen-lg mx-auto'>
+        <div className='flex flex-col gap-6'>
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+        </div>
+      </div>
+    </Container>
   );
 };
 
--- app/components/listings/ListingHead/ListingHead.tsx
+++ app/components/listings/ListingHead/ListingHead.tsx
@@ -1,4 +1,22 @@
-const ListingHead = () => {
+'use client';
+
+import { SafeUser } from '@/app/types';
+
+interface ListingHeadProps {
+  title: string;
+  imageSrc: string;
+  locationValue: string;
+  id: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingHead = ({
+  title,
+  imageSrc,
+  locationValue,
+  id,
+  currentUser,
+}: ListingHeadProps) => {
   return <div>gfgf</div>;
 };
 
