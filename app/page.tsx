5369c77e434f61065adee4fb36971e60f0f49574
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-14 15:47:06 +0100

    fix: update page to fetch listings from mongo

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
+import getListings from './actions/getListings';
 import Container from './components/container/Container';
 import EmptyState from './components/emptystate/EmptyState';
+import ListingCard from './components/listings/ListingCard/ListingCard';
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return <EmptyState showReset />;
   }
 
   return (
     <Container>
       <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-        dfd
+        {listings.map((listing) => (
+          <ListingCard key={listing.id} />
+        ))}
       </div>
     </Container>
   );
