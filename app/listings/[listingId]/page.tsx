30ae16f987f40826fe5685ce8721f5a71fa99523
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-14 19:42:52 +0100

    feat: import listing client component

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,5 +1,7 @@
+import getCurrentUser from '@/app/actions/getCurrentUser';
 import getListingById from '@/app/actions/getListingById';
 import EmptyState from '@/app/components/emptystate/EmptyState';
+import ListingClient from '@/app/components/listings/ListingClient.tsx/ListingClient';
 
 interface ListingPageProps {
   listingId?: string;
@@ -7,6 +9,7 @@ interface ListingPageProps {
 
 const ListingPage = async ({ params }: { params: ListingPageProps }) => {
   const listing = await getListingById(params);
+  const currentUser = await getCurrentUser();
 
   if (!listing) {
     return <EmptyState />;
@@ -15,6 +18,7 @@ const ListingPage = async ({ params }: { params: ListingPageProps }) => {
   return (
     <div>
       <h1>{listing.title}</h1>
+      <ListingClient listing={listing} currentUser={currentUser} />
     </div>
   );
 };
