cbeb4f0a378541dfd42f7c9528e91e3c1d5dad92
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-15 19:13:30 +0100

    fix: update listing card to use sanitised reservation type

--- app/components/listings/ListingCard/ListingCard.tsx
+++ app/components/listings/ListingCard/ListingCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import useCountries from '@/app/hooks/useCountries';
-import { SafeListing, SafeUser } from '@/app/types';
+import { SafeListing, SafeReservation, SafeUser } from '@/app/types';
 import { Reservation } from '@prisma/client';
 import { useRouter } from 'next/navigation';
 import { useCallback, useMemo } from 'react';
@@ -12,7 +12,7 @@ import Button from '../../button/Button';
 
 interface ListingCardProps {
   data: SafeListing;
-  reservation?: Reservation;
+  reservation?: SafeReservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
   actionLabel?: string;
@@ -68,29 +68,29 @@ const ListingCard = ({
   return (
     <div
       onClick={() => router.push(`/listings/${data.id}`)}
-      className='col-span-1 cursor-pointer group'
+      className="col-span-1 cursor-pointer group"
     >
-      <div className='flex flex-col gap-2 w-full'>
-        <div className='aspect-square w-full relative overflow-hidden rounded-xl'>
+      <div className="flex flex-col gap-2 w-full">
+        <div className="aspect-square w-full relative overflow-hidden rounded-xl">
           <Image
-            alt='Listing'
+            alt="Listing"
             src={data.imageSrc}
-            className='object-cover h-full w-full group-hover:scale-110 transition'
+            className="object-cover h-full w-full group-hover:scale-110 transition"
             fill
           />
-          <div className='absolute top-3 right-3'>
+          <div className="absolute top-3 right-3">
             <HeartButton listingId={data.id} currentUser={currentUser} />
           </div>
         </div>
-        <div className='font-semibold text-lg'>
+        <div className="font-semibold text-lg">
           {location?.region}, {location?.label}
         </div>
-        <div className='font-light text-neutral-500'>
+        <div className="font-light text-neutral-500">
           {reservationDate || data.category}
         </div>
-        <div className='flex flex-row items-center gap-1'>
-          <div className='font-semibold'>$ {price}</div>
-          {!reservation && <div className='font-light'>night</div>}
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">night</div>}
         </div>
         {onAction && actionLabel && (
           <Button
