bf87d39df467323ef450bab7488056b59629539a
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-15 13:56:04 +0100

    feat: work on listing header to show listing details

--- app/components/listings/ListingHead/ListingHead.tsx
+++ app/components/listings/ListingHead/ListingHead.tsx
@@ -1,6 +1,10 @@
 'use client';
 
+import useCountries from '@/app/hooks/useCountries';
 import { SafeUser } from '@/app/types';
+import Heading from '../../heading/Heading';
+import Image from 'next/image';
+import HeartButton from '../../button/HeartButton/HeartButton';
 
 interface ListingHeadProps {
   title: string;
@@ -17,7 +21,29 @@ const ListingHead = ({
   id,
   currentUser,
 }: ListingHeadProps) => {
-  return <div>gfgf</div>;
+  const { getByValue } = useCountries();
+
+  const location = getByValue(locationValue);
+
+  return (
+    <>
+      <Heading
+        title={title}
+        subtitle={`${location?.region}, ${location?.label}`}
+      />
+      <div className="w-full h-[60vh] overflow-hidden rounded-xl relative">
+        <Image
+          alt="Image"
+          src={imageSrc}
+          fill
+          className="object-cover w-full"
+        />
+        <div className="absolute top-5 right-5">
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ListingHead;
