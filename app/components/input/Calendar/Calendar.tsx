7ee0fb1ee50706787dda1f82c37f1ba0e35f48d7
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-15 15:42:04 +0100

    feat: add date range component with styling

--- app/components/input/Calendar/Calendar.tsx
+++ app/components/input/Calendar/Calendar.tsx
@@ -1,7 +1,16 @@
 'use client';
 
-const Calendar = () => {
-  return <div>hello</div>;
+import { Range, RangeKeyDict, DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+interface CalendarProps {
+  value: Range;
+  disabledDates?: Date[];
+  onChange: (value: RangeKeyDict) => void;
+}
+
+const Calendar = ({ value, disabledDates, onChange }: CalendarProps) => {
+  return <DateRange />;
 };
 
 export default Calendar;
