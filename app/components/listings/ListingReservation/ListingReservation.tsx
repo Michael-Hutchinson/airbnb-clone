30d3e2b915f430f43630e25294301f209f00defd
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-15 15:15:35 +0100

    feat: create interface for reservations

--- app/components/listings/ListingReservation/ListingReservation.tsx
+++ app/components/listings/ListingReservation/ListingReservation.tsx
@@ -1,6 +1,26 @@
 'use client';
 
-const ListingReservation = () => {
+import { Range } from 'react-date-range';
+
+interface ListingReservationProps {
+  price: number;
+  dateRange: Range;
+  totalPrice: number;
+  disabledDates: Date[];
+  disabled: boolean;
+  onSubmit: () => void;
+  onChange: (value: Range) => void;
+}
+
+const ListingReservation = ({
+  price,
+  dateRange,
+  totalPrice,
+  disabledDates,
+  disabled,
+  onSubmit,
+  onChange,
+}: ListingReservationProps) => {
   return (
     <div>
       <h1>ListingReservation</h1>
