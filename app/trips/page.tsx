f6f3b0c208f53d142518389a31701d7a934c1298
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-15 18:53:52 +0100

    feat: add not logged in state to trips page

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,7 +1,13 @@
 import EmptyState from '../components/emptystate/EmptyState';
+import getCurrentUser from '../actions/getCurrentUser';
+import getReservations from '../actions/getReservations';
 
-const TripsPage = () => {
-  return <EmptyState />;
+const TripsPage = async () => {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return <EmptyState title="Unauthorised" subtitle="Please login" />;
+  }
+  return <h1>trips page</h1>;
 };
 
 export default TripsPage;
