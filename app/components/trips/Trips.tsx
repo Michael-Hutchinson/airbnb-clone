7b2bce120553ffc8b3e1914f556668bb2325e210
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-15 19:13:59 +0100

    feat: display current reservations on page

--- app/components/trips/Trips.tsx
+++ app/components/trips/Trips.tsx
@@ -3,6 +3,11 @@
 import { SafeReservation, SafeUser } from '@/app/types';
 import Container from '../container/Container';
 import Heading from '../heading/Heading';
+import { useRouter } from 'next/navigation';
+import { useCallback, useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import ListingCard from '../listings/ListingCard/ListingCard';
 
 interface TripsProps {
   reservations: SafeReservation[];
@@ -10,9 +15,46 @@ interface TripsProps {
 }
 
 const Trips = ({ reservations, currentUser }: TripsProps) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState('');
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
+
+      axios
+        .delete(`/api/reservations/${id}`)
+        .then(() => {
+          toast.success('Reservation cancelled');
+          router.refresh();
+        })
+        .catch((error) => {
+          toast.error(error.response.data.error);
+        })
+        .finally(() => {
+          setDeletingId('');
+        });
+    },
+    [router],
+  );
+
   return (
     <Container>
       <Heading title="Trips" subtitle="Where are you going on holiday!" />
+      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {reservations.map((reservation) => (
+          <ListingCard
+            key={reservation.id}
+            data={reservation.listing}
+            reservation={reservation}
+            actionId={reservation.id}
+            onAction={onCancel}
+            disabled={deletingId === reservation.id}
+            actionLabel="Cancel reservation"
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
     </Container>
   );
 };
