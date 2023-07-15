c1a8dff5577fbe07b8af7cc6b74eedc08db059a1
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-15 15:18:47 +0100

    feat: display price on reservation section

--- app/components/listings/ListingReservation/ListingReservation.tsx
+++ app/components/listings/ListingReservation/ListingReservation.tsx
@@ -22,8 +22,10 @@ const ListingReservation = ({
   onChange,
 }: ListingReservationProps) => {
   return (
-    <div>
-      <h1>ListingReservation</h1>
+    <div className="bg-white rounded-lg border-[1px] border-neutral-200 overflow-hidden">
+      <div className="flex flex-row items-center gap-1 p-4">
+        <div className="text-2xl font-semibold">$ {price}</div>
+      </div>
     </div>
   );
 };
