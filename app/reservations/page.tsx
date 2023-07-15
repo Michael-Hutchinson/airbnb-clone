b136a41e5d39dec33f24a9c3617a92211b4f2194
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-15 21:18:34 +0100

    feat: check for current user and existing reservations

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -1,4 +1,22 @@
-const ReservationsPage = () => {
+import EmptyState from '../components/emptystate/EmptyState';
+import getCurrentUser from '../actions/getCurrentUser';
+import getReservations from '../actions/getReservations';
+
+const ReservationsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return <EmptyState title="Unauthorised" subtitle="Please login" />;
+  }
+
+  const reservations = await getReservations({
+    authorId: currentUser.id,
+  });
+
+  if (reservations.length === 0) {
+    return <EmptyState title="No reservations" subtitle="Please create one" />;
+  }
+
   return (
     <div>
       <h1>Reservations Page</h1>
