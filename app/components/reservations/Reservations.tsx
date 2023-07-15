5a1f8973c953b320cbb6f461bc4d34e4340523c8
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-15 21:40:28 +0100

    feat: add the cancel function

--- app/components/reservations/Reservations.tsx
+++ app/components/reservations/Reservations.tsx
@@ -9,7 +9,35 @@ import Heading from '../heading/Heading';
 import Container from '../container/Container';
 import ListingCard from '../listings/ListingCard/ListingCard';
 
+interface ReservationsProps {
+  reservations: SafeReservation[];
+  currentUser: SafeUser | null;
+}
+
 const Reservations = () => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState('');
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
+
+      axios
+        .delete(`/api/reservations/${id}`)
+        .then(() => {
+          toast.success('Reservation cancelled');
+          router.refresh();
+        })
+        .catch(() => {
+          toast.error('Something went wrong');
+        })
+        .finally(() => {
+          setDeletingId('');
+        });
+    },
+    [router],
+  );
+
   return (
     <Container>
       <Heading title="Reservations" subtitle="Bookings on your properties" />
