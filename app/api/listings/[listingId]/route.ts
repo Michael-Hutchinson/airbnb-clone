8e638e800a129ec90cf142f1adcb8f631c931f84
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-17 17:36:42 +0100

    feat: create route to delete properties

---
+++ app/api/listings/[listingId]/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server';
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import prisma from '@/app/libs/prismadb';
+
+interface Listing {
+  listingId?: string;
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: Listing },
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== 'string') {
+    throw new Error('Invalid listingId');
+  }
+
+  const listing = await prisma.listing.deleteMany({
+    where: {
+      id: listingId,
+      userId: currentUser.id,
+    },
+  });
+
+  return NextResponse.json(listing);
+}
