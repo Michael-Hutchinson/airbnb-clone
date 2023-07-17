0964126469f88a28408892a2b2a6e36c7d92e38b
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-17 16:45:24 +0100

    wip: create placeholder page for favourites

---
+++ app/favourites/page.tsx
@@ -0,0 +1,8 @@
+import EmptyState from '../components/emptystate/EmptyState';
+import getCurrentUser from '../actions/getCurrentUser';
+
+const FavouritesPage = () => {
+  return <div>hello</div>;
+};
+
+export default FavouritesPage;
