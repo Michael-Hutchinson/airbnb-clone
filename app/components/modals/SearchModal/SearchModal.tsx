597cc9770df06b94d322a9991ee8d9b354cdba48
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-17 17:49:28 +0100

    feat: create modal and import into layout

--- app/components/modals/SearchModal/SearchModal.tsx
+++ app/components/modals/SearchModal/SearchModal.tsx
@@ -1,5 +1,19 @@
+'use client';
+
+import useSearchModal from '@/app/hooks/useSearchModal';
+import Modal from '../Modal';
+
 const SearchModal = () => {
-  return <div>hello</div>;
+  const searchModal = useSearchModal();
+  return (
+    <Modal
+      title="Filters"
+      actionLabel="Search"
+      isOpen={searchModal.isOpen}
+      onClose={searchModal.onClose}
+      onSubmit={searchModal.onClose}
+    />
+  );
 };
 
 export default SearchModal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import LoginModal from './components/modals/LoginModal/LoginModal';
 import RegisterModal from './components/modals/RegisterModal/RegisterModal';
 import ToasterProvider from './providers/ToasterProvider';
 import getCurrentUser from './actions/getCurrentUser';
+import SearchModal from './components/modals/SearchModal/SearchModal';
 
 export const metadata = {
   title: 'Airbnb',
@@ -23,14 +24,15 @@ export default async function RootLayout({
 }) {
   const currentUser = await getCurrentUser();
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
         <RentModal />
+        <SearchModal />
         <RegisterModal />
         <LoginModal />
         <Navbar currentUser={currentUser} />
-        <div className='pb-20 pt-28'>{children}</div>
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
