d7e1e93ecc7a6b95568bbd8ac4d0006f6417ac73
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-17 16:54:44 +0100

    feat: show empty state if no favourite listings exist

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -1,7 +1,20 @@
 import EmptyState from '../components/emptystate/EmptyState';
 import getCurrentUser from '../actions/getCurrentUser';
+import getFavouriteListings from '../actions/getFavouriteListings';
 
-const FavouritesPage = () => {
+const FavouritesPage = async () => {
+  const listings = await getFavouriteListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <EmptyState
+        title="No favourites found!"
+        subtitle="Looks like you have no favourite listings"
+      />
+    );
+  }
+
   return <div>hello</div>;
 };
 
