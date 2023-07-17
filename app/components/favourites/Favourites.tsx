ec274414d6ddd177f80cd76167c1a082aa624f8d
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-17 17:04:38 +0100

    feat: add listing card to favourites component to show users favourites

--- app/components/favourites/Favourites.tsx
+++ app/components/favourites/Favourites.tsx
@@ -3,6 +3,7 @@
 import { SafeListing, SafeUser } from '@/app/types';
 import Container from '../container/Container';
 import Heading from '../heading/Heading';
+import ListingCard from '../listings/ListingCard/ListingCard';
 
 interface FavouritesProps {
   listings: SafeListing[];
@@ -16,6 +17,15 @@ const Favourites = ({ listings, currentUser }: FavouritesProps) => {
         title="Favourites"
         subtitle="List of places you have favourited!"
       />
+      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing) => (
+          <ListingCard
+            key={listing.id}
+            data={listing}
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
     </Container>
   );
 };
